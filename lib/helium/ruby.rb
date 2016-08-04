b02c4da7177c31fc0be9cba6df7e3d47bc44bb3b
Andrew Allen <allenan@helium.com>
2016-08-04 11:30:03 -0700

    boilerplate Ruby gem from bundler

---
+++ lib/helium/ruby.rb
@@ -0,0 +1,7 @@
+require "helium/ruby/version"
+
+module Helium
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/helium/ruby/version.rb
@@ -0,0 +1,5 @@
+module Helium
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
