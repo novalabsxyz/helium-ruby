861552230e00e23634972360e63d6912f59d94f5
Andrew Allen <allenan@helium.com>
2016-08-10 14:18:57 -0400

    fix up test coverage, add some roadmap items

--- spec/helium/client_spec.rb
+++ spec/helium/client_spec.rb
@@ -6,4 +6,8 @@ describe Helium::Client do
   it 'has an api key' do
     expect(client.api_key).to eq(API_KEY)
   end
+
+  it 'does not show the api key when inspecting the object' do
+    expect(client.inspect).not_to include(API_KEY)
+  end
 end
