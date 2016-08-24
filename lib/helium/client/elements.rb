00bea06ea071ce7ac3a8257602943049289b84a5
Jared Morrow <jaredmorrow@users.noreply.github.com>
2016-08-24 11:06:24 -0600

    Add sensor relationships for elements

--- lib/helium/client/elements.rb
+++ lib/helium/client/elements.rb
@@ -8,6 +8,18 @@ module Helium
       def element(id)
         Element.find(id, client: self)
       end
+
+      def element_sensors(element)
+        path = "/element/#{element.id}/relationships/sensor"
+        response = get(path)
+        sensors_data = JSON.parse(response.body)["data"]
+
+        sensors = sensors_data.map do |sensor|
+          Sensor.new(client: self, params: sensor)
+        end
+
+        return sensors
+      end
     end
   end
 end
