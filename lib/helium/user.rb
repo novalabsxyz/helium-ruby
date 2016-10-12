bd059dfa4b409692024f7595ce0677ff8f770461
Andrew Allen <allenan@helium.com>
2016-10-12 10:36:10 -0700

    add User#pending_invite?

--- lib/helium/user.rb
+++ lib/helium/user.rb
@@ -1,12 +1,14 @@
 module Helium
   class User < Resource
-    attr_reader :name, :email
+    attr_reader :name, :email, :pending_invite
+    alias_method :pending_invite?, :pending_invite
 
     def initialize(opts = {})
       super(opts)
 
-      @name  = @params.dig('attributes', 'name')
-      @email = @params.dig('meta', 'email')
+      @name           = @params.dig('attributes', 'name')
+      @email          = @params.dig('meta', 'email')
+      @pending_invite = @params.dig('meta', 'pending_invite')
     end
 
     # TODO can probably generalize this a bit more
