0cca1ba12cd9078f0483d84fd8d51e9c451e171a
Lane Seppala <lseppala@users.noreply.github.com>
2017-03-03 10:47:47 -0700

    add/replace/remove labels for a sensor (#45)

--- spec/helium/sensor_spec.rb
+++ spec/helium/sensor_spec.rb
@@ -44,6 +44,167 @@ describe Helium::Sensor, '#labels' do
   end
 end
 
+describe Helium::Sensor, '#add_labels' do
+  let(:client) { Helium::Client.new(api_key: API_KEY) }
+
+  context "with a single label" do
+    use_cassette 'sensor/add_single_label'
+    it 'adds a single label to a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create a new label
+      new_label_a = client.create_label(name: "A Test Label")
+
+      # add one label to sensor
+      new_sensor.add_labels(new_label_a)
+
+      # expect the label to be assigned to the label
+      all_label_ids = [new_label_a.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a].each(&:destroy)
+    end
+  end
+
+  context "with multiple labels" do
+    use_cassette 'sensor/add_multiple_labels'
+    it 'adds multiple labels to a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create new labels
+      new_label_a = client.create_label(name: "A Test Label")
+      new_label_b = client.create_label(name: "B Test Label")
+
+      # add labels to sensor
+      new_sensor.add_labels([new_label_a, new_label_b])
+
+      # expect the label to be assigned to the label
+      all_label_ids = [new_label_a.id, new_label_b.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a, new_label_b].each(&:destroy)
+    end
+  end
+end
+
+describe Helium::Sensor, '#replace_labels' do
+  let(:client) { Helium::Client.new(api_key: API_KEY) }
+
+  context "with a single label" do
+    use_cassette 'sensor/replace_single_label'
+    it 'replaces a single label associated with a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create new labels
+      new_label_a = client.create_label(name: "A Test Label")
+      new_label_b = client.create_label(name: "B Test Label")
+
+      # add one label to sensor
+      new_sensor.add_labels(new_label_a)
+
+      # replace the sensor's label
+      new_sensor.replace_labels(new_label_b)
+
+      # expect the label to be assigned to the label
+      all_label_ids = [new_label_b.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a, new_label_b].each(&:destroy)
+    end
+  end
+
+  context "with multiple labels" do
+    use_cassette 'sensor/replace_multiple_labels'
+    it 'replaces multiple labels associated with a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create new labels
+      new_label_a = client.create_label(name: "A Test Label")
+      new_label_b = client.create_label(name: "B Test Label")
+      new_label_c = client.create_label(name: "C Test Label")
+
+      # add label to sensor
+      new_sensor.add_labels(new_label_a)
+      # Replace with labels
+      new_sensor.replace_labels([new_label_b, new_label_c])
+
+      # expect the labels to be assigned to the label
+      all_label_ids = [new_label_b.id, new_label_c.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a, new_label_b, new_label_c].each(&:destroy)
+    end
+  end
+end
+
+describe Helium::Sensor, '#remove_labels' do
+  let(:client) { Helium::Client.new(api_key: API_KEY) }
+
+  context "with a single label" do
+    use_cassette 'sensor/remove_single_label'
+    it 'removes a single label to a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create new labels
+      new_label_a = client.create_label(name: "A Test Label")
+      new_label_b = client.create_label(name: "B Test Label")
+
+      # add labels to sensor
+      new_sensor.add_labels([new_label_a, new_label_b])
+
+      # delete one of the sensor's label
+      new_sensor.remove_labels(new_label_a)
+
+      # expect the label to be assigned to the label
+      all_label_ids = [new_label_b.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a, new_label_b].each(&:destroy)
+    end
+  end
+
+  context "with multiple labels" do
+    use_cassette 'sensor/remove_multiple_labels'
+    it 'removes multiple labels associated with a sensor' do
+
+      # create a sensor
+      new_sensor = client.create_sensor(name: "Test Sensor A")
+
+      # create new labels
+      new_label_a = client.create_label(name: "A Test Label")
+      new_label_b = client.create_label(name: "B Test Label")
+      new_label_c = client.create_label(name: "C Test Label")
+
+      # add label to sensor
+      new_sensor.add_labels([new_label_a, new_label_b, new_label_c])
+      # Remove labels
+      new_sensor.remove_labels([new_label_b, new_label_c])
+
+      # expect the labels to be assigned to the label
+      all_label_ids = [new_label_a.id]
+      expect(new_sensor.labels.map(&:id)).to contain_exactly(*all_label_ids)
+
+      # clean up
+      [new_sensor, new_label_a, new_label_b, new_label_c].each(&:destroy)
+    end
+  end
+end
+
 describe Helium::Sensor, '#device_configuration' do
   let(:client) { Helium::Client.new(api_key: API_KEY) }
   let(:sensor) { client.sensor("aba370be-837d-4b41-bee5-686b0069d874") }
